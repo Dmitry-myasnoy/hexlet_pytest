265d8c8205d627663c49ded2486e7bb457651c24
Dima <dmitrybertov222@gmail.com>
2024-11-19 22:14:05 +0300

    Добавлены фикстуры для реверса

--- tests/test_example.py
+++ tests/test_example.py
@@ -5,3 +5,15 @@ def test_reverse():
 
 def test_reverse_for_empty_string():
     assert('') == ''
+
+def test_reverse():
+    # Чтение исходного текста
+    with open('tests/fixtures/input_text.txt', 'r', encoding='utf8') as f:
+        input_text = f.read().strip()
+
+    # Чтение ожидаемого результата
+    with open('tests/fixtures/expected_output.txt', 'r', encoding='utf8') as f:
+        expected_output = f.read().strip()
+
+    # Проверка работы функции reverse
+    assert reverse(input_text) == expected_output
